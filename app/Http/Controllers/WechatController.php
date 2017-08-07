735286c56b83d848294c2a519023cf2599c94da5
javaxx <136222866@qq.com>
2017-08-07 10:28:08 +0800

    initWeixin

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -22,43 +22,16 @@ class WechatController extends Controller
             // ...
         ];
         $app = new Application($options);
-    //    $server = $app->server;
-       // $server->serve()->send();
+        // 从项目实例中得到服务端应用实例。
+        $server = $app->server;
+        $server->setMessageHandler(function ($message) {
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            return "您好！欢迎关注我!";
+        });
+        $response = $server->serve();
+        $response->send(); // Laravel 里请使用：return $response;
 
-// 从项目实例中得到服务端应用实例。
-        //$server->serve()->send();
-//        $server->setMessageHandler(function ($message) {
-//            switch ($message->MsgType) {
-//                case 'event':
-//                    return '收到事件消息';
-//                    break;
-//                case 'text':
-//                    return '收到文字消息';
-//                    break;
-//                case 'image':
-//                    return '收到图片消息';
-//                    break;
-//                case 'voice':
-//                    return '收到语音消息';
-//                    break;
-//                case 'video':
-//                    return '收到视频消息';
-//                    break;
-//                case 'location':
-//                    return '收到坐标消息';
-//                    break;
-//                case 'link':
-//                    return '收到链接消息';
-//                    break;
-//                // ... 其它消息
-//                default:
-//                    return '收到其它消息';
-//                    break;}
-//        });
-//        $response = $server->serve();
-//
-//        dd($response);
-//        $response->send(); // Laravel 里请使用：return $response;
 
     }
 }
